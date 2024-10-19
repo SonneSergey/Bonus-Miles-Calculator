37254c8a2f06955802548280f0c216ff60235880
Sergey_Sonne <berit2008@mail.ru>
2024-10-19 21:34:11 +0300

    Bonus Miles Calculator

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println("Количество бонусных миль:" + bonusMiles);
+    }
+}
