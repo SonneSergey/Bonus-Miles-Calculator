7b2b7f098a6020ba70a3d878190c11cf6668acbe
Sergey_Sonne <berit2008@mail.ru>
2024-10-20 00:29:51 +0300

    Update Main.java with new features

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // Объявление переменной для хранения стоимости билета
         int ticketPrice = 15000;
+        // Объявление переменной для хранения количества рублей для одной бонусной мили
         int rublesPerMile = 20;
+        // Рассчёт количества бонусных миль
         int bonusMiles = ticketPrice / rublesPerMile;
+        // Вывод результата на экран
         System.out.println("Количество бонусных миль:" + bonusMiles);
     }
 }
